d69d1b51db23bfc5f824e6805207447c51ea6501
3JloyCJloHuk <kostyaopanasuk@gmail.com>
2023-02-09 22:02:33 +0200

    clicker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		count: 0,
+	};
+
+	handleClickPlus = (a) => {
+		this.setState((prev) => {
+			return { count: prev.count + 1 }
+
+		})
+	}
+	handleClickMinus = (a) => {
+		this.setState((prev) => {
+			return { count: prev.count - 1 }
+		})
+	}
+
+	render() {
+		return (<div>
+			<button onClick={this.handleClickMinus}>-</button>
+			<p>{this.state.count}</p>
+			<button onClick={this.handleClickPlus}>+</button>
+		</div>)
+	}
 }
-
 export default App;
