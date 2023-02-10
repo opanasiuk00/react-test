0dc9dcb7dbb841f3ab083f0bc9f034e34943afb9
3JloyCJloHuk <kostyaopanasuk@gmail.com>
2023-02-10 16:32:03 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -2,27 +2,63 @@ import React, { Component } from 'react';
 
 class App extends Component {
 	state = {
-		count: 0,
-	};
+		timer: 0,
+		status: false
+	}
 
-	handleClickPlus = (a) => {
-		this.setState((prev) => {
-			return { count: prev.count + 1 }
+	componentDidMount() {
+		this.time = localStorage.getItem('react-time');
+		const data = this.time ? JSON.parse(this.time) : { timer: 0, status: false };
+		this.setState({
+			timer: data.timer,
+			status: data.status
+		});
+		if (data.status === true) {
+			this.startTimer();
+		}
+	}
 
+	componentDidUpdate() {
+		this.json = JSON.stringify(this.state);
+		localStorage.setItem('react-time', this.json);
+	}
+
+	handleClickStartStop = () => {
+		if (this.state.status === false) {
+			this.startTimer();
+		} else {
+			clearInterval(this.timerId);
+		}
+		this.setState(({ status }) => {
+			return { status: !status };
 		})
 	}
-	handleClickMinus = (a) => {
-		this.setState((prev) => {
-			return { count: prev.count - 1 }
-		})
+
+	startTimer = () => {
+		this.timerId = setInterval(() => {
+			this.setState(({ timer }) => {
+				return { timer: timer + 1 }
+			});
+		}, 1000);
 	}
+
+	resetTimer = () => {
+		clearInterval(this.timerId);
+		this.setState({
+			timer: 0,
+			status: false
+		});
+	}
+
 
 	render() {
-		return (<div>
-			<button onClick={this.handleClickMinus}>-</button>
-			<p>{this.state.count}</p>
-			<button onClick={this.handleClickPlus}>+</button>
-		</div>)
+		return (
+			<div className='app'>
+				<button onClick={this.handleClickStartStop}>{this.state.status ? 'stop' : 'start'}</button>
+				<span>{this.state.timer}</span>
+				<button onClick={this.resetTimer}>reset</button>
+			</div>
+		)
 	}
 }
 export default App;
